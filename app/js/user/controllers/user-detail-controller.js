1834a190301a6baf6564d4418dfc359872333a8b
Meechyst <meechysm@windowslive.com>
2017-07-16 14:23:27 +0300

    fix user map (using ng-src instead of jquery)

--- app/js/user/controllers/user-detail-controller.js
+++ app/js/user/controllers/user-detail-controller.js
@@ -2,16 +2,22 @@
 
   angular.module('myApp.user').controller('UserDetailController', UserDetailController);
 
-  UserDetailController.$inject = ['_user'];
+  UserDetailController.$inject = ['_user', '$sce'];
 
-  function UserDetailController(_user) {
+  function UserDetailController(_user, $sce) {
     var vm = this;
     vm.userNotFound = false;
+    vm.showMap = false;
 
-    if (_user.id > 0) {
-      vm.user = _user;
-    } else {
-      vm.userNotFound = true;
+
+    if (_user.id > 0) { vm.user = _user; } else { vm.userNotFound = true; };
+
+    vm.userGeo = vm.user.address.geo.lat + "," + vm.user.address.geo.lng;
+    vm.mapUrl = $sce.trustAsResourceUrl
+    ("https://www.google.com/maps/embed/v1/place?key=AIzaSyDuTD9neX7jtRe-YcqDAvApGPA5qHX6kjk&q="+ vm.userGeo);
+    console.log(vm.mapUrl);
+    vm.toggleMap = function() {
+      vm.showMap = !vm.showMap;
     }
   }
 })();
